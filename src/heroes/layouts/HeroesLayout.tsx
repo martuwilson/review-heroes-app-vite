8a7db817ba9cf765c831a080ed23c83990cd95cb
martuwilson <williner.dev@gmail.com>
2025-08-28 16:52:51 -0300

    feat: mejorar el diseño de HeroesLayout y optimizar la carga de SearchPage y AdminLayout

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router"
+
 export const HeroesLayout = () => {
   return (
-    <div>
+    <div className="p-20 bg-blue-400">
       <h1>Heroes Layout</h1>
+      <ul>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/heroes/1">Hero</Link>
+        </li>
+        <li>
+            <Link to="/search">Search</Link>
+        </li>
+        <li>
+            <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -2,9 +2,13 @@ import { createBrowserRouter } from "react-router";
 import HomePage from '../heroes/pages/home/HomePage';
 import AdminPage from "@/admin/pages/AdminPage";
 import HeroPage from "@/heroes/pages/hero/HeroPage";
-import SearchPage from "@/heroes/pages/search/SearchPage";
+//import SearchPage from "@/heroes/pages/search/SearchPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
-import AdminLayout from "@/admin/layouts/AdminLayout";
+//import AdminLayout from "@/admin/layouts/AdminLayout";
+import { lazy } from "react";
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
+const AdminLayout = lazy(() => import('@/admin/layouts/AdminLayout'));
 
 export const appRouter = createBrowserRouter([
     {
