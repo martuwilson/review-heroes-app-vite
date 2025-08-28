e7b12ce6bb24afaa35774f980cd34c5a3f22f098
martuwilson <williner.dev@gmail.com>
2025-08-28 16:44:43 -0300

    feat: agregar layouts de Heroes y Admin, y actualizar enrutamiento

---
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -0,0 +1,7 @@
+export const HeroesLayout = () => {
+  return (
+    <div>
+      <h1>Heroes Layout</h1>
+    </div>
+  )
+}
--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -3,22 +3,38 @@ import HomePage from '../heroes/pages/home/HomePage';
 import AdminPage from "@/admin/pages/AdminPage";
 import HeroPage from "@/heroes/pages/hero/HeroPage";
 import SearchPage from "@/heroes/pages/search/SearchPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import AdminLayout from "@/admin/layouts/AdminLayout";
 
 export const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage />
+        element: <HeroesLayout />,
+        children:
+            [
+                {
+                    index: true,
+                    element: <HomePage />
+                },
+                {
+                    path: "/hero/1",
+                    element: <HeroPage />
+                },
+                {
+                    path: "/search",
+                    element: <SearchPage />
+                }
+            ]
     },
+    
     {
         path: "/admin",
-        element: <AdminPage />
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <AdminPage />
+            }
+        ]
     },
-    {
-        path: "/hero/1",
-        element: <HeroPage />
-    },
-    {
-        path: "/search",
-        element: <SearchPage />
-    }
 ])
